61564448cfd0cb15b46ada72751376733b7a13bc
Aerossky <riskyradeonrx560@gmail.com>
2023-05-15 08:53:22 +0700

    First Commit

---
+++ BmiBros/BmiBrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmiBrosApp.swift
+//  BmiBros
+//
+//  Created by MacBook Pro on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BmiBrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
