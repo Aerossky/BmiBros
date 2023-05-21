aa2e06009bd1ff5335bb326a1487775793f268ad
Aerossky <riskyradeonrx560@gmail.com>
2023-05-21 22:23:17 +0700

    memperbaiki tampilan sign in dan sign up

--- BmiBros/View/SignInView.swift
+++ BmiBros/View/SignInView.swift
@@ -4,53 +4,27 @@
 //
 //  Created by MacBook Pro on 21/05/23.
 //
-
 import SwiftUI
 
 struct SignInView: View {
-    // Deklarasi variable
-        @State private var email: String = ""
-        @State private var password: String = ""
-        @State private var isPasswordVisible: Bool = false // Tambahkan variabel keadaan ini
-        
-        var body: some View {
-            VStack(){
-                Spacer()
-                Image("login")
-                    .frame(width: 262, height: 263)
-                // Text Login
-                Text("Login")
-                    .font(.custom("Poppins-bold", size: 24))
-                    .frame(width: 300,alignment: .leading)
-                // Text Field Email
-                TextField("Email", text: $email)
-                    .padding()
-                    .frame(width: 300,height: 50)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color(UIColor(hex: "#ffffff")))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.black, lineWidth: 1)
-                            )
-                    )
-                // Text Field Password
-                HStack {
-                    //condition when password image get hit
-                    if isPasswordVisible {
-                        TextField("Password", text: $password)
-                    } else {
-                        SecureField("Password", text: $password)
-                    }
-                    Button(action: {
-                        isPasswordVisible.toggle()
-                    }) {
-                        Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
-                            .foregroundColor(.gray)
-                    }
-                }
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isPasswordVisible: Bool = false
+
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("login")
+                .resizable()
+                .frame(width: 262, height: 263)
+            Spacer()
+            Text("Login")
+                .font(.custom("Poppins-Bold", size: 24))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            
+            TextField("Email", text: $email)
                 .padding()
-                .frame(width: 300,height: 50)
                 .background(
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color(UIColor(hex: "#ffffff")))
@@ -59,59 +33,83 @@ struct SignInView: View {
                                 .stroke(Color.black, lineWidth: 1)
                         )
                 )
-                // Text Field Forgot Password?
-                Text("Forgot Password?")
-                    .frame(width: 300,alignment: .trailing)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal)
+            
+            HStack {
+                if isPasswordVisible {
+                    TextField("Password", text: $password)
+                } else {
+                    SecureField("Password", text: $password)
+                }
                 
-                Spacer()
-                
-                //Button login
                 Button(action: {
-                    // Aksi tombol login
+                    isPasswordVisible.toggle()
                 }) {
-                    Text("Sign In")
-                        .font(.custom("Poppins-SemiBold", size: 16))
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 300)
-                        .background(Color(UIColor(hex: "#98A8F8")))
-                        .cornerRadius(10)
-                    
+                    Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
+                        .foregroundColor(.gray)
                 }
-                
-                //sign in with apple
-                Button(action: {
-                    // Aksi tombol sign in with apple
-                }) {
-                    HStack {
-                        Image(systemName: "apple.logo")
-                            .resizable()
-                            .frame(width: 18, height: 22)
-                        Text("Sign in with Apple")
-                            .foregroundColor(.white)
-                            .font(.system(size: 16))
-                            .fontWeight(.bold)
-                            .frame(height: 22)
-                            .padding(.top, 5)
-                    }
-                    .foregroundColor(Color(UIColor(hex: "#ffffff")))
-                    .padding()
-                    .frame(width: 300)
-                    .background(Color.black)
-                    .cornerRadius(10)
-                }
-                
-                //text around here
-                HStack {
-                    Text("New around here? ")
-                        .foregroundColor(Color.black)
-                        .padding(.trailing, -5)
-                    Text("Sign Up")
-                        .foregroundColor(Color(UIColor(hex: "#98A8F8")))
-                }
-                .padding(.vertical, 10)
             }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(UIColor(hex: "#ffffff")))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+            )
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+            Spacer()
+            
+            Button(action: {
+                // Aksi tombol login
+            }) {
+                Text("Sign In")
+                    .font(.custom("Poppins-SemiBold", size: 16))
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color(UIColor(hex: "#98A8F8")))
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal)
+            
+            Button(action: {
+                // Aksi tombol sign in with apple
+            }) {
+                HStack {
+                    Image(systemName: "applelogo")
+                        .resizable()
+                        .frame(width: 18, height: 22)
+                    Text("Sign in with Apple")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16, weight: .bold))
+                        .frame(height: 22)
+                        .padding(.top, 5)
+                }
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.black)
+                .cornerRadius(10)
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Text("New around here? ")
+                    .foregroundColor(.black)
+                    .font(.custom("Poppins-Light", size: 15))
+                
+                Text("Sign Up")
+                    .foregroundColor(Color(UIColor(hex: "#98A8F8")))
+                    .font(.custom("Poppins-Light", size: 15))
+            }
+            .padding(.vertical, 10)
         }
+        .padding(.vertical)
+    }
 }
 
 struct SignInView_Previews: PreviewProvider {
---
+++ BmiBros/View/SignUpView.swift
@@ -0,0 +1,134 @@
+//
+//  SignUpView.swift
+//  BmiBros
+//
+//  Created by MacBook Pro on 21/05/23.
+//
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var username: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var gender: String = ""
+    @State private var isPasswordVisible: Bool = false
+    @State private var selectedGender = 0
+    
+    var genderOptions = ["Male", "Female"]
+    @State private var isSignUpButtonAnimating = false
+    
+    var body: some View {
+        VStack {
+            Image("signup")
+                .frame(width: 262, height: 263)
+                .animation(.spring()) // Animasi untuk gambar
+            
+            Spacer()
+            
+            Text("Sign Up")
+                .font(.custom("Poppins-Bold", size: 24))
+                .frame(width: 360, alignment: .leading)
+                .animation(.spring()) // Animasi untuk judul
+            
+            TextField("Username", text: $username)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor(hex: "#ffffff")))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                )
+                .padding(.horizontal)
+            
+            TextField("Email", text: $email)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(UIColor(hex: "#ffffff")))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                )
+                .padding(.horizontal)
+            
+            HStack {
+                if isPasswordVisible {
+                    TextField("Password", text: $password)
+                } else {
+                    SecureField("Password", text: $password)
+                }
+                Button(action: {
+                    isPasswordVisible.toggle()
+                }) {
+                    Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(UIColor(hex: "#ffffff")))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+            )
+            .padding(.horizontal)
+            
+            Picker("Gender", selection: $selectedGender) {
+                ForEach(0..<genderOptions.count) { index in
+                    Text(genderOptions[index])
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .frame(height: 50)
+            .padding(.horizontal)
+            
+            VStack(alignment: .leading) {
+                Text("By signing up, you have read and agreed to our")
+                    .font(.custom("Poppins-Light", size: 15))
+                    .padding(.horizontal)
+                
+                Text("Terms & conditions")
+                    .foregroundColor(Color(UIColor(hex: "#6D85FD")))
+                    .font(.custom("Poppins-Bold", size: 15))
+                    .padding(.horizontal)
+            }
+            
+            Spacer()
+            
+            VStack {
+                Button(action: {
+                    // Aksi tombol login
+                    isSignUpButtonAnimating.toggle() // Mengubah status animasi tombol
+                }) {
+                    Text("Sign Up")
+                        .font(.custom("Poppins-SemiBold", size: 16))
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color(UIColor(hex: "#98A8F8")))
+                        .cornerRadius(10)
+                       }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("Already have an account?")
+                    Text("Sign In")
+                        .foregroundColor(Color(UIColor(hex: "#98A8F8")))
+                }
+                .font(.custom("Poppins-Light", size: 15))
+                .padding(.top)
+            }
+        }
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
