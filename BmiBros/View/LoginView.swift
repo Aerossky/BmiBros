d9df61ae25d92871fae7e290372f8f8091a4a065
Aerossky <riskyradeonrx560@gmail.com>
2023-05-21 15:54:40 +0700

    menambahkan icon mata untuk password, dan logic ketika di tekan password terlihat

--- BmiBros/View/LoginView.swift
+++ BmiBros/View/LoginView.swift
@@ -6,12 +6,11 @@
 //
 
 import SwiftUI
-
 struct LoginView: View {
-    //deklarasi variable
+    // Deklarasi variable
     @State private var email: String = ""
     @State private var password: String = ""
-    
+    @State private var isPasswordVisible: Bool = false // Tambahkan variabel keadaan ini
     
     var body: some View {
         VStack(){
@@ -35,27 +34,39 @@ struct LoginView: View {
                         )
                 )
             // Text Field Password
-            SecureField("Password", text: $password)
-                .padding()
-                .frame(width: 300,height: 50)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color(UIColor(hex: "#ffffff")))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                )
+            HStack {
+                //condition when password image get hit
+                if isPasswordVisible {
+                    TextField("Password", text: $password)
+                } else {
+                    SecureField("Password", text: $password)
+                }
+                Button(action: {
+                    isPasswordVisible.toggle()
+                }) {
+                    Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
+            .frame(width: 300,height: 50)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(UIColor(hex: "#ffffff")))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.black, lineWidth: 1)
+                    )
+            )
             // Text Field Forgot Password?
             Text("Forgot Password?")
-            .frame(width: 300,alignment: .trailing)
+                .frame(width: 300,alignment: .trailing)
             
-           Spacer()
-
+            Spacer()
+            
             //Button login
             Button(action: {
-           
-
+                // Aksi tombol login
             }) {
                 Text("Sign In")
                     .font(.custom("Poppins-SemiBold", size: 16))
@@ -66,16 +77,15 @@ struct LoginView: View {
                     .cornerRadius(10)
                 
             }
+            
             //sign in with apple
             Button(action: {
-                
+                // Aksi tombol sign in with apple
             }) {
-
                 HStack {
                     Image(systemName: "apple.logo")
                         .resizable()
                         .frame(width: 18, height: 22)
-                      
                     Text("Sign in with Apple")
                         .foregroundColor(.white)
                         .font(.system(size: 16))
@@ -83,27 +93,22 @@ struct LoginView: View {
                         .frame(height: 22)
                         .padding(.top, 5)
                 }
-                    .foregroundColor(Color(UIColor(hex: "#ffffff")))
-                    .padding()
-                    .frame(width: 300)
-                    .background(Color.black)
-                    .cornerRadius(10)
+                .foregroundColor(Color(UIColor(hex: "#ffffff")))
+                .padding()
+                .frame(width: 300)
+                .background(Color.black)
+                .cornerRadius(10)
             }
+            
             //text around here
-            HStack{
-                //text 1
+            HStack {
                 Text("New around here? ")
                     .foregroundColor(Color.black)
                     .padding(.trailing, -5)
-                //text 2
                 Text("Sign Up")
                     .foregroundColor(Color(UIColor(hex: "#98A8F8")))
-            }.padding(.vertical,10)
-            
-           
-
-            
-            
+            }
+            .padding(.vertical, 10)
         }
     }
 }
