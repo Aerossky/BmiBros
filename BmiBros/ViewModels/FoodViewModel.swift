02214301681534eee90af5004c12c156544ba6d4
Aerossky <riskyradeonrx560@gmail.com>
2023-05-22 20:12:29 +0700

    membuat FoodViewModel

---
+++ BmiBros/ViewModels/FoodViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  FoodViewModel.swift
+//  BmiBros
+//
+//  Created by MacBook Pro on 22/05/23.
+//
+
+import Foundation
+
+class FoodViewModel: ObservableObject {
+    @Published var foods: [Food] = []
+    
+    init() {
+        fetchFoods()
+    }
+    
+    func fetchFoods() {
+        // Simulasi pengambilan data dari sumber eksternal (misalnya, API atau database)
+        let sampleFoods = [
+            Food(
+                image: "food Image",
+                name: "Salad Babi",
+                description: "Salad isi babi ala Joseph yang enaknya wauwwwww makjos slebew (aku diajarin Rey)",
+                cal: 478,
+                prot: 30,
+                carbs: 25,
+                fat: 12
+            ),
+            Food(
+                image: "food Image",
+                name: "Sushi Babi",
+                description: "Sushi isi babi ala Joseph yang enaknya wauwwwww makjos slebew (aku diajarin Rey)",
+                cal: 1,
+                prot: 2,
+                carbs: 3,
+                fat: 4
+            ),
+            Food(
+                image: "food Image",
+                name: "masak Babi",
+                description: "Joseph isi babi ala Joseph yang enaknya wauwwwww makjos slebew (aku diajarin Rey)",
+                cal: 5,
+                prot: 6,
+                carbs: 7,
+                fat: 8
+            )
+        ]
+        
+        // Assign data yang diambil ke properti foods
+        self.foods = sampleFoods
+    }
+}
