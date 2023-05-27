c450bddfb4c94b4d168ecfc91d0d43362dbfc2ac
Jovan <jovanlemuel9103@gmail.com>
2023-05-27 22:51:29 +0700

    make profile view, make watch os target and watch os content view

---
+++ BmiBros Watch App Watch App/BmiBros_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmiBros_Watch_AppApp.swift
+//  BmiBros Watch App Watch App
+//
+//  Created by MacBook Pro on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BmiBros_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
