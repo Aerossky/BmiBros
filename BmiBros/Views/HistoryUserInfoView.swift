f6073fa2f1480b5793ecb9c5d0380e03cda6255c
JosephWijaya34 <josephwijaya01@gmail.com>
2023-06-04 18:38:02 +0700

    history done , filter done, show berdasarkan user id yang login done

--- BmiBros/Views/HistoryUserInfoView.swift
+++ BmiBros/Views/HistoryUserInfoView.swift
@@ -8,30 +8,90 @@
 import SwiftUI
 
 struct HistoryUserInfoView: View {
-    @EnvironmentObject var session: SessionManager
-    @EnvironmentObject var viewModel: UserViewModel
-    @EnvironmentObject var inputViewModel: InputViewModel
-    @EnvironmentObject var appEnvironment: AppEnvironment
+    @State private var selectedOption = 0
+    let options = ["BMI History", "Calories History"]
     
     var body: some View {
         NavigationView{
+            VStack {
+                
+                Picker("Pilih Konten", selection: $selectedOption) {
+                    ForEach(options.indices, id: \.self) { index in
+                        Text(options[index]).tag(options[index])
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal, 50)
+                //            Spacer()
+                if selectedOption == 0 {
+                    BMIHistoryView()
+                } else {
+                    CaloriesHistoryView()
+                }
+            }
+        }
+    }
+    
+    struct BMIHistoryView: View {
+        @EnvironmentObject var session: SessionManager
+        @EnvironmentObject var viewModel: UserViewModel
+        @EnvironmentObject var inputViewModel: InputViewModel
+        @EnvironmentObject var appEnvironment: AppEnvironment
+        
+        var body: some View{
             List {
                 ForEach(viewModel.userInfos) { userInfo in
                     if userInfo.userId == viewModel.loggedInUser?.id.uuidString {
-                        HStack {
-                            Text("BMI")
-                            Text("\(Int(userInfo.bmi))")
-                                .font(.headline)
-                            Spacer()
-                            Text("CALS")
-                            Text("\(Int(userInfo.calories))")
+                        if userInfo.bmi != 0 || userInfo.bmi > 0 {
+                            HStack {
+                                Text("Age")
+                                Text("\(Int(userInfo.age))")
+                                    .font(.headline)
+                                Spacer()
+                                Text("BMI")
+                                Text("\(Int(userInfo.bmi))")
+                            }
+                            .padding()
+                            .listRowSeparator(.hidden)
+                            .listRowInsets(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                            .background(Color(UIColor(hex: "#F7F7F7")))
+                            .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
                         }
-                        .padding()
-                        .listRowSeparator(.hidden)
-                        .listRowInsets(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
-                        .background(Color(UIColor(hex: "#F7F7F7")))
-                        .cornerRadius(20)
-                        .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .accentColor(.clear)
+        }
+    }
+    
+    struct CaloriesHistoryView: View {
+        @EnvironmentObject var session: SessionManager
+        @EnvironmentObject var viewModel: UserViewModel
+        @EnvironmentObject var inputViewModel: InputViewModel
+        @EnvironmentObject var appEnvironment: AppEnvironment
+        
+        var body: some View{
+            List {
+                ForEach(viewModel.userInfos) { userInfo in
+                    if userInfo.userId == viewModel.loggedInUser?.id.uuidString {
+                        if userInfo.calories != 0 || userInfo.calories > 0 {
+                            HStack {
+                                Text("Age")
+                                Text("\(Int(userInfo.age))")
+                                    .font(.headline)
+                                Spacer()
+                                Text("Cals")
+                                Text("\(Int(userInfo.calories))")
+                            }
+                            .padding()
+                            .listRowSeparator(.hidden)
+                            .listRowInsets(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                            .background(Color(UIColor(hex: "#F7F7F7")))
+                            .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.2), radius: 4, x: 0, y: 2)
+                        }
                     }
                 }
             }
